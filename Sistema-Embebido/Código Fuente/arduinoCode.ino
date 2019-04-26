d04b9841a98eac3591810907400e6ad04a09c86e
Federico Gomez Markowicz <fede.markoo@gmail.com>
2019-04-25 23:53:31 -0300

    Update arduinoCode.ino

--- Sistema-Embebido/Código Fuente/arduinoCode.ino
+++ Sistema-Embebido/Código Fuente/arduinoCode.ino
@@ -1,24 +1,45 @@
 #include "SoftwareSerial.h"
 #include "HX711.h"
 
-
 /////// constantes
-int SIN_ACCION = 0;
-int SACAR_CONTENIDO = 1;
-/////// constantes
+#define SIN_ACCION           0;
+#define SACAR_CONTENIDO      1;
+#define MOVER_BRAZO          2;
+#define BAJAR_SINFIN         3;
+#define RETORNAR_A_REPOSO    4;
 
+/////// Sensores
+#define humedad             68;
+#define temperatura         69;
+#define presencia           48;
+#define btPairing           49;
+#define balanza             50; // no se usa como pin para sensor, se utiliza para enviar al celular
 
-SoftwareSerial bt(9, 10); // RX, TX
-HX711 bal(5, 6);
+/////// Actuadores
+#define sinFin              44;
+#define zumbador            13;
+#define ledEncendido        12;
+#define ledConectadoBT      11;
+#define ledSirviendo        10;
+#define ledDisponible        9;
+#define ledCantNoDisponible  8;
 
-bool isInAction = false;
+#define alimentacionBT      23;
+#define tiempoZumbador     150;
+
+SoftwareSerial bt(20, 21); // RX, TX
+HX711 bal(62, 63);
+
+int accion = SIN_ACCION;
 int pesoADepositar = 0;
+int zumbadorTime = 0;
 
 float calibration_factor = 2230;
 
 void setup() {
   bt.begin(9600);
-  bt.print("AT + NAMELaJarraDeAda");
+  bt.print("AT + NAMELaJarraDeAda"); // porque es todo de ada?
+  //porque la flaca fue la primer persona en programar
   bt.print("AT + PINpass");
 
   bal.set_scale();
@@ -39,35 +60,62 @@ void setup() {
   pinMode(ledDisponible       , OUTPUT);
   pinMode(ledCantNoDisponible , OUTPUT);
 
+  // necesario para desconectar "a la fuerza" el bt
+  pinMode(alimentacionBT      , OUTPUT);
+
+  digitalWrite(ledEncendido, HIGH);
 }
 
 void loop() {// no usar delay()
-  switch (accion) {
+  digitalWrite(alimentacionBT, !digitalRead(btPairing));
 
-    case SIN_ACCION:
-      leerBT();
-      break;
+  if (btConectado) {
+    digitalWrite(ledConectadoBT, HIGH);
+    switch (accion) {
+      case SIN_ACCION:
+        leerBT();
+        break;
 
-    case SACAR_CONTENIDO:
-      if (digitalRead(presencia) == HIGH) {
-        pesoBalanza = leerBalanza();
-        if (pesoADepositar == 0)
-          leerPesoADepositar();
-        if (pesoRequerido == 0)
-          pesoRequerido = pesoADepositar - pesoBalanza;
-        if (pesoRequerido != pesoBalanza)
-          analogWrite(sinFin, porcentaje(50)); //0-255
-        else
-          accion = SIN_ACCION;
-      }
-      break;
+      case SACAR_CONTENIDO:
+        if (digitalRead(presencia) == HIGH) {
+          pesoBalanza = leerBalanza();
+          if (pesoADepositar == 0)
+            leerPesoADepositar();
+          if (pesoRequerido == 0)
+            pesoRequerido = pesoADepositar - pesoBalanza;
+          if (pesoRequerido != pesoBalanza)
+            analogWrite(sinFin, porcentaje(50)); //0-255
+          else {
+            analogWrite(sinFin, 0); //0-255
+            pesoRequerido = 0;
+            pesoADepositar = 0;
+            zumbadorTime = milis();
+            accion = RETORNAR_A_REPOSO;
+          }
+        }
+        break;
 
-    case MOVER_BRAZO:
-    case BAJAR_SINFIN:
-      //UNIMPLEMENTED
-      break;
+      case MOVER_BRAZO:
+        //UNIMPLEMENTED
+        accion = BAJAR_SINFIN;
+        break;
+
+      case BAJAR_SINFIN:
+        //SEMI-IMPLEMENTED
+        enviarHumedad();
+        accion = SACAR_CONTENIDO;
+        break;
+
+      case RETORNAR_A_REPOSO:
+        //UNIMPLEMENT
+        accion = SIN_ACCION;
+        break;
+    }
+    alertas();
+    enviarInfo();
   }
-  alertas();
+  else
+    digitalWrite(ledConectadoBT, (milis() % 450 > 200));
 }
 
 void leerPesoADepositar() {
@@ -95,13 +143,43 @@ float leerBalanza() {
   return peso;
 }
 
-//////////////////////////// ALERTAS
+//////////////////////////// Info
+void enviarInfo() {
+  bt.print(1)
+  bt.print(accion);
 
-void alertas() {
-  alertaHumedad();
+  if (accion == SACAR_CONTENIDO)
+  {
+    bt.print(ledSirviendo);
+    bt.print((milis() % 500 > 250));
+  }
 
+  bt.print(ledDisponible);
+  bt.print(accion == SIN_ACCION);
+
+  bt.print(ledCantNoDisponible);
+  bt.print(pesoBalanza > pesoRequerido);
 }
 
-void alertaHumedad() {
+void enviarHumedad() {
+  bt.print(balanza);
   bt.print(analogRead(humedad));
+}
+
+//////////////////////////// ALERTAS
+void alertas() {
+  if (accion == SACAR_CONTENIDO)
+    digitalWrite(ledSirviendo, (milis() % 500 > 250));
+
+  if (!btConectado)
+    digitalWrite(ledConectadoBT, (milis() % 450 > 200));
+  else
+    digitalWrite(ledConectadoBT, HIGH);
+
+  digitalWrite(ledDisponible, accion == SIN_ACCION);
+
+  if (zumbadorTime - milis() < tiempoZumbador)
+    digitalWrite(zumbador, milis() % (zumbadorTime / 4) < (zumbadorTime / 4) * 0.7); // deberia hacer un pipipipi....
+
+  digitalWrite(ledCantNoDisponible, pesoBalanza > pesoRequerido);
 }
