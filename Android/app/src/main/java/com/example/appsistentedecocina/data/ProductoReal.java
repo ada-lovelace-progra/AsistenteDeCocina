ba5d7f0dc0294b1f27c43b1d9f0bd5375b8d7546
Federico, Gomez Markowicz <fmarkowicz@tgv.com.ar>
2019-05-25 18:24:30 -0300

    ClasesParaBD

---
+++ Android/app/src/main/java/com/example/appsistentedecocina/data/ProductoReal.java
@@ -0,0 +1,49 @@
+package com.example.appsistentedecocina.data;
+
+public class ProductoReal {
+    private int id;
+    private String marca;
+    private int barcode;
+    private int productoid;
+    private int cantidad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(int barcode) {
+        this.barcode = barcode;
+    }
+
+    public int getProductoid() {
+        return productoid;
+    }
+
+    public void setProductoid(int productoid) {
+        this.productoid = productoid;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
