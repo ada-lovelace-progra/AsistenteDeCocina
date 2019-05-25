cce1a15e0a74630d382e145ae736e4d34c9070e9
pokengineer <vfacuu@gmail.com>
2019-05-25 17:04:05 -0300

    hay un dialogo pero no funciona

--- Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
+++ Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,6 +16,8 @@ public class List_Ingrediente extends AppCompatActivity {
 
     private ListView listview;
     private AdaptadorIngrediente adapter;
+
+    private Button nuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +30,9 @@ public class List_Ingrediente extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No esta implementado, bancame", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
 
