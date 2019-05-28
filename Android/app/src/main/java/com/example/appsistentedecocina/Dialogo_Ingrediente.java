da56f445a1218c07e35e8df082b687add9beb1e3
pokengineer <vfacuu@gmail.com>
2019-05-28 20:07:23 -0300

    dialogo para agregar ingredientes

--- Android/app/src/main/java/com/example/appsistentedecocina/Dialogo_Ingrediente.java
+++ Android/app/src/main/java/com/example/appsistentedecocina/Dialogo_Ingrediente.java
@@ -1,13 +1,66 @@
 package com.example.appsistentedecocina;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Dialogo_Ingrediente extends AppCompatActivity {
+//public class Dialogo_Ingrediente extends AppCompatActivity {
+public class Dialogo_Ingrediente extends Activity {
+
+    private Button btnOK, btnCancelar;
+    private EditText txtIngrediente, txtCantidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //esto es para hacer que sea igual de ancho que el parent,
+        // no me termina de convencer si queda bien o no, pero que se yo, de ultima lo volamos
         setContentView(R.layout.activity_dialogo__ingrediente);
+        WindowManager.LayoutParams param = getWindow().getAttributes();
+        param.width = WindowManager.LayoutParams.FILL_PARENT;
+        getWindow().setAttributes((android.view.WindowManager.LayoutParams) param );
+
+        //va a buscar los elementros del layout que me sirven
+        btnOK = (Button) findViewById(R.id.btnOK);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        txtIngrediente=(EditText)findViewById(R.id.txtDestino);
+        txtCantidad=(EditText)findViewById(R.id.txtCant);
+
+        //seteo el listener a la antigua
+        btnOK.setOnClickListener(botonesListeners);
+        btnCancelar.setOnClickListener(botonesListeners);
     }
+
+
+
+    private View.OnClickListener botonesListeners = new View.OnClickListener()
+    {
+        public void onClick(View v) {
+
+            switch (v.getId())
+            {
+                case R.id.btnOK:
+
+                    Toast.makeText(getApplicationContext(), "todavia no lo implemente, mañana lo hago", Toast.LENGTH_SHORT).show();
+                    //finish();
+                    break;
+
+                case R.id.btnCancelar:
+                    finish();
+                    break;
+                default:
+                    Toast.makeText(getApplicationContext(),"Error en Listener de botones",Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    };
 }
--- Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
+++ Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+
+import static android.app.PendingIntent.getActivity;
 
 public class List_Ingrediente extends AppCompatActivity {
 
@@ -35,8 +39,11 @@ public class List_Ingrediente extends AppCompatActivity {
 //                        .setAction("Action", null).show();
                 Intent intent;
                 intent=new Intent(List_Ingrediente.this, Dialogo_Ingrediente.class);
-                //aca tiene que ir al dialogo pero no lo se metera
+//                //aca tiene que ir al dialogo pero no lo se metera
                 startActivity(intent);
+
+
+
 
             }
         });
