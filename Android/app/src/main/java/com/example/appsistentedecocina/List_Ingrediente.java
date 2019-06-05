e7bc8dda6b9aa64a4ae1311ac0a4411e91ef4a05
pokengineer <vfacuu@gmail.com>
2019-06-05 16:17:22 -0300

    cambie varios dialogos, borre cosas extra

--- Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
+++ Android/app/src/main/java/com/example/appsistentedecocina/List_Ingrediente.java
@@ -1,17 +1,26 @@
 package com.example.appsistentedecocina;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,14 +30,18 @@ public class List_Ingrediente extends AppCompatActivity {
 
     private ListView listview;
     private AdaptadorIngrediente adapter;
-
-    private Button nuevo;
+    private Intent intent;
+    private ArrayList<Ingrediente> listaIngredientes = new ArrayList<Ingrediente>();
+    private String nuevo_ingrediente = "";
+    private int nueva_cantidad = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list__ingrediente);
+        setTitle("Lista de Ingredientes");
 
+        final Context context = this;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -37,25 +50,79 @@ public class List_Ingrediente extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "No esta implementado, bancame", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                Intent intent;
-                intent=new Intent(List_Ingrediente.this, Dialogo_Ingrediente.class);
-//                //aca tiene que ir al dialogo pero no lo se metera
-                startActivity(intent);
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Nuevo Ingrediente");
+                //contexto
+                LinearLayout layout = new LinearLayout(context);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                // Set up the input
+                final TextView ingrediente = new TextView(context);
+                final EditText input = new EditText(context);
+                final TextView cantidad = new TextView(context);
+                final EditText input2 = new EditText(context);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                ingrediente.setText(" ingrediente:");
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
+                cantidad.setText(" cantidad (gr):");
+                input2.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_CLASS_NUMBER);
+                //lo meto en el layout este
+                layout.addView(ingrediente);
+                layout.addView(input);
+                layout.addView(cantidad);
+                layout.addView(input2);
+                //lo seteo
+                builder.setView(layout);
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (! nuevo_ingrediente.equals("")){
+                            nuevo_ingrediente = input.getText().toString();
+                            nueva_cantidad = Integer.parseInt(input2.getText().toString());
+                            add_ingrediente();
 
-
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
 
             }
         });
 
-        listview = (ListView)findViewById(R.id.ingrediente_list);
-        ArrayList<Ingrediente> listaIngredientes = new ArrayList<Ingrediente>();
+        listview = (ListView) findViewById(R.id.ingrediente_list);
 
-        listaIngredientes.add(new Ingrediente("arroz",150));
-        listaIngredientes.add(new Ingrediente("yerba",500));
+        listaIngredientes.add(new Ingrediente("arroz", 150));
+        listaIngredientes.add(new Ingrediente("yerba", 500));
 
         adapter = new AdaptadorIngrediente(this, listaIngredientes);
         listview.setAdapter(adapter);
     }
 
+    private void add_ingrediente() {
+
+        if (nuevo_ingrediente.equals("") || nueva_cantidad <= 0) {
+            Toast.makeText(getApplicationContext(), "pusiste valores invalidos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        listaIngredientes.add(new Ingrediente(nuevo_ingrediente, nueva_cantidad));
+        adapter = new AdaptadorIngrediente(this, listaIngredientes);
+        listview.setAdapter(adapter);
+
+        nuevo_ingrediente = "";
+        nueva_cantidad = 0;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter = new AdaptadorIngrediente(this, listaIngredientes);
+        listview.setAdapter(adapter);
+    }
 }
